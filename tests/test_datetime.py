dbf65f4af2e3e5d1cdc5c0b39da43d135a97071e
Charles-Axel Dein <charles@uber.com>
2016-02-12 17:54:01 +0100

    Fix mocked DateTimeType timezone behavior

--- tests/test_datetime.py
+++ tests/test_datetime.py
@@ -3,7 +3,8 @@
 from datetime import datetime, timedelta
 import sys
 
-from dateutil.tz import gettz, tzutc
+from dateutil.tz import gettz
+import mock
 import pytest
 
 from schematics.exceptions import ConversionError, ValidationError
@@ -296,3 +297,25 @@ def test_validate_tz():
     with pytest.raises(ValidationError):
         field.validate_tz(dt_nyc)
 
+
+@mock.patch('schematics.types.base.random.randrange')
+def test_get_mocked_datetime_allow(mock_randrange):
+    # Forces a timezone to always be added
+    mock_randrange.return_value = 1
+    field = DateTimeType(tzd='allow')
+    assert field._mock().tzinfo is not None
+
+
+def test_get_mocked_datetime_reject():
+    field = DateTimeType(tzd='reject')
+    assert field._mock().tzinfo is None
+
+
+def test_get_mocked_datetime_require():
+    field = DateTimeType(tzd='require')
+    assert field._mock().tzinfo is not None
+
+
+def test_get_mocked_datetime_utc():
+    field = DateTimeType(tzd='utc')
+    assert field._mock().tzinfo == DateTimeType.UTC
